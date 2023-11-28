c9007a6e6c88ad2fca86e48369d48ead7dd38dae
szt2szaviv <szt2szaviv@vasvari.org>
2023-11-28 11:28:27 +0100

    példa

--- LakasHirdetes/LakasHirdetes/Context/ModelBuilderExtension.cs
+++ LakasHirdetes/LakasHirdetes/Context/ModelBuilderExtension.cs
@@ -1,8 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace LakasHirdetes.Context
 {
     public class ModelBuilderExtension
     {
+        /*public static void Seed(this ModelBuilder modelBuilder)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="János",
+                    LastName="Jegy",
+                    BirthsDay=new DateTime(2022,10,10),
+                    SchoolYear=9,
+                    SchoolClass = SchoolClassType.ClassA,
+                    EducationLevel="érettségi"
+                },
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Szonja",
+                    LastName="Stréber",
+                    BirthsDay=new DateTime(2021,4,4),
+                    SchoolYear=10,
+                    SchoolClass = SchoolClassType.ClassB,
+                    EducationLevel="érettségi"
+                }
+            };
+
+            modelBuilder.Entity<Student>().HasData(students);
+        }*/
     }
 }
